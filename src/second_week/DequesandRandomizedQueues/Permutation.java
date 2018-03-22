a54f23aeb45f1dc53f31a99b962bfd056f989f6c
Elder <solomon7dun@gmail.com>
2018-03-23 02:32:29 +0300

    added Permutation class

---
+++ src/second_week/DequesandRandomizedQueues/Permutation.java
@@ -0,0 +1,24 @@
+package second_week.DequesandRandomizedQueues;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Permutation {
+
+    public static void main(String[] args){
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> q = new RandomizedQueue<String>();
+
+        while(!StdIn.hasNextLine() && StdIn.isEmpty());
+        String str = StdIn.readLine();
+        String[] splited = str.split(" ");
+
+        for(String x: splited){
+            q.enqueue(x);
+        }
+
+        for(int i = 0;i < k;i++){
+            StdOut.println(q.dequeue());
+        }
+    }
+}
